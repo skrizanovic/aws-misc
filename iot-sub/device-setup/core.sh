ea1ac362f58e48c949ae1936784009fedd873f17
skrizanovic <skrizanovic@users.noreply.github.com>
2021-09-09 06:37:48 +1000

    Delete iot-sub directory

--- iot-sub/device-setup/core.sh
+++
@@ -1,3 +0,0 @@
-sudo adduser -u 1444 gcc-docker
-
-sudo usermod -aG docker gcc-docker
--- iot-sub/iot-infra/bin/iot-infra.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { IotInfraStack } from '../lib/iot-infra-stack';
-
-const app = new cdk.App();
-new IotInfraStack(app, 'IotInfraStack');
